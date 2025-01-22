a28f6a45454b6394e68a2e3fcb902e9a503567d8
barathbalasubramanian <105224717+barathbalasubramanian@users.noreply.github.com>
2025-01-22 15:20:38 +0530

    cat-Type

--- models/Category.js
+++ models/Category.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
     categoryName: { type: String, required: true },
+    categoryType: { type: String, required: true,
+        enum: ['Saree', 'Others'],
+    },
     categotyImage: { type: String, required: true },
     isDeleted: { type: Boolean, default: false },
 });
