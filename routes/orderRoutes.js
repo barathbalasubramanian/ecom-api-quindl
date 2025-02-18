d9176d3f0bfeb33f17c3e599a9f6055a81e182fb
Surya J <143687264+sarnsurya@users.noreply.github.com>
2025-02-18 12:56:10 +0530

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -8,6 +8,7 @@ router.get('/:id', getOrderById);
 router.put('/:id/pay', updateOrderToPaid);
 router.put('/:id/deliver', updateOrderToDelivered);
 router.get('/', getAllOrders);
+router.put('/:id/shipping-status', updateShippingStatus);
 router.post('/:orderId/items/:itemId/refund', refundOrderItem);
 
 module.exports = router;
