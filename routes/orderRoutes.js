964847d1e60e8b4656557631743e85c4ec7be6f1
Surya J <143687264+sarnsurya@users.noreply.github.com>
2025-02-18 11:37:30 +0530

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const { createOrder, getOrderById, updateOrderToPaid, updateOrderToDelivered, getMyOrders, getAllOrders, refundOrderItem } = require('../controllers/OrderController');
-const { protect, admin } = require('../middlewares/authMiddleware');
+const { createOrder, getOrderById, updateOrderToPaid, updateOrderToDelivered, getAllOrders } = require('../controllers/OrderController');
 
 const router = express.Router();
 
-router.post('/', protect, createOrder);
-router.get('/myorders', protect, getMyOrders);
-router.get('/:id', protect, getOrderById);
-router.put('/:id/pay', protect, updateOrderToPaid);
-router.put('/:id/deliver', protect, updateOrderToDelivered);
-router.get('/', protect, getAllOrders);
-router.put('/refund/:itemId', protect, refundOrderItem);
+router.post('/', createOrder);
+router.get('/:id', getOrderById);
+router.put('/:id/pay', updateOrderToPaid);
+router.put('/:id/deliver', updateOrderToDelivered);
+router.get('/', getAllOrders);
 
 module.exports = router;
