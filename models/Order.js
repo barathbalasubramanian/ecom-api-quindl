ff5f9f7b32a8ab633949a338e81982222116ac18
barathbalasubramanian <105224717+barathbalasubramanian@users.noreply.github.com>
2025-02-18 11:16:30 +0530

    remove user

--- models/Order.js
+++ models/Order.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     orderItems: [{ type: mongoose.Schema.Types.ObjectId, ref: 'OrderItem' }],
     shippingAddress: {
         firstName: { type: String, required: true },
