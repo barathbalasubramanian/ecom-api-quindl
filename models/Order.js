8c9661816d2050f580a51e391f829883d78693d8
Surya J <143687264+sarnsurya@users.noreply.github.com>
2025-02-19 20:44:13 +0530

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-    orderItems: [{ type: mongoose.Schema.Types.ObjectId, ref: 'OrderItem', required: true }],
+    orderItems: [{ type: mongoose.Schema.Types.ObjectId, ref: 'OrderItem' }],
     shippingAddress: {
         firstName: { type: String, required: true },
         lastName: { type: String, required: true },
@@ -14,17 +14,15 @@ const orderSchema = new mongoose.Schema({
         phoneNumber: { type: String, required: true }
     },
     paymentMethod: { type: String, required: true },
-    paymentResult: { id: { type: String }, status: { type: String }, update_time: { type: String }, email_address: { type: String }
-    },
     itemsPrice: { type: Number, required: true },
     taxPrice: { type: Number, required: true },
     shippingPrice: { type: Number, required: true },
     totalPrice: { type: Number, required: true },
     isPaid: { type: Boolean, default: false },
+    shippingStatus: { type: String },
     paidAt: { type: Date },
     isDelivered: { type: Boolean, default: false },
     deliveredAt: { type: Date },
-    orderStatus: { type: String, enum: ['Pending', 'Processing', 'Shipped', 'Delivered', 'Cancelled'], default: 'Pending' },
     createdAt: { type: Date, default: Date.now }
 });
 
