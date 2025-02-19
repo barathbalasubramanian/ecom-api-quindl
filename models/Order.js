28e45a1e555becdef35e901c53a55968f10e79ba
Surya J <143687264+sarnsurya@users.noreply.github.com>
2025-02-19 12:38:39 +0530

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,7 +1,5 @@
-const mongoose = require('mongoose');
-
 const orderSchema = new mongoose.Schema({
-    orderItems: [{ type: mongoose.Schema.Types.ObjectId, ref: 'OrderItem' }],
+    orderItems: [{ type: mongoose.Schema.Types.ObjectId, ref: 'OrderItem', required: true }],
     shippingAddress: {
         firstName: { type: String, required: true },
         lastName: { type: String, required: true },
@@ -13,17 +11,15 @@ const orderSchema = new mongoose.Schema({
         state: { type: String, required: true },
         phoneNumber: { type: String, required: true }
     },
-    paymentMethod: { type: String, required: true },
-    itemsPrice: { type: Number, required: true },
-    taxPrice: { type: Number, required: true },
-    shippingPrice: { type: Number, required: true },
-    totalPrice: { type: Number, required: true },
+    paymentMethod: { type: String, required: true, default: 'COD' },
+    itemsPrice: { type: Number, required: true, default: 0 },
+    taxPrice: { type: Number, required: true, default: 0 },
+    shippingPrice: { type: Number, required: true, default: 0 },
+    totalPrice: { type: Number, required: true, default: 0 },
     isPaid: { type: Boolean, default: false },
-    shippingStatus: { type: String },
+    shippingStatus: { type: String, enum: ['Processing', 'Shipped', 'Delivered', 'Cancelled', 'Refunded'],default: 'Processing' },
     paidAt: { type: Date },
     isDelivered: { type: Boolean, default: false },
     deliveredAt: { type: Date },
     createdAt: { type: Date, default: Date.now }
 });
-
-module.exports = mongoose.model('Order', orderSchema);
